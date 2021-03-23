8b13e9d97eb1cd8a14d813aea5f7fea0a5f044b0
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-23 13:31:49 +0900

    add freezed

--- lib/models/app/token.dart
+++ lib/models/app/token.dart
@@ -2,7 +2,11 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'token.freezed.dart';
 
+part 'token.g.dart';
+
 @freezed
-abstract class Token with _$Token {
+class Token with _$Token {
   const factory Token(String value) = _Token;
+
+  factory Token.fromJson(dynamic value) => Token(value as String);
 }
