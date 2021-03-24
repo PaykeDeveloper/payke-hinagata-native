da0b9c20b718e01c6cfa79406d04d34f7d6c7276
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-24 21:49:28 +0900

    state_notifier導入

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_state_notifier/flutter_state_notifier.dart';
-import 'package:native_app/base/api/client.dart';
-import 'package:native_app/models/app/language.dart';
 import 'package:native_app/models/app/provider_state.dart';
 import 'package:native_app/models/app/token.dart';
-import 'package:native_app/providers/app/api_client.dart';
-import 'package:native_app/providers/app/language.dart';
-import 'package:native_app/providers/app/login.dart';
-import 'package:native_app/providers/app/token.dart';
+import 'package:native_app/providers/providers.dart';
 import 'package:native_app/ui/pages/common/home.dart';
 import 'package:native_app/ui/pages/common/loading.dart';
 import 'package:native_app/ui/pages/common/login.dart';
@@ -19,16 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        StateNotifierProvider<TokenProvider, ProviderState<Token?>>(
-            create: (context) => TokenProvider()),
-        StateNotifierProvider<LanguageProvider, ProviderState<Language?>>(
-            create: (context) => LanguageProvider()),
-        StateNotifierProvider<ApiClientProvider, ApiClient>(
-            create: (context) => ApiClientProvider()),
-        StateNotifierProvider<LoginProvider, ProviderState<Login>>(
-            create: (context) => LoginProvider()),
-      ],
+      providers: getProviders(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
