c5ce6ac9b357f5b7f92d0d72457655f09f644c6f
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-25 16:19:17 +0900

    state_notifier導入

--- test/base/preference_test.dart
+++ test/base/preference_test.dart
@@ -2,33 +2,110 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:native_app/base/preference.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// ignore: avoid_classes_with_only_static_members
+class TestPreference {
+  static final boolValue = PreferenceProperty<bool>('boolValue');
+  static final intValue = PreferenceProperty<int>('intValue');
+  static final doubleValue = PreferenceProperty<double>('doubleValue');
+  static final stringValue = PreferenceProperty<String>('stringValue');
+  static final stringListValue = PreferenceListProperty('stringListValue');
+}
+
 void main() {
   group('Preference Tests', () {
-    final token = Preference.backendToken;
-
-    test('値が設定できる。', () async {
+    test('bool', () async {
       SharedPreferences.setMockInitialValues({});
-      final result = await token.set('test');
-      expect(result, true);
+      final pref = TestPreference.boolValue;
+      final startValue = await pref.get();
+      expect(startValue, null);
+
+      await pref.set(true);
+      final createValue = await pref.get();
+      expect(createValue, true);
+
+      await pref.set(false);
+      final updateValue = await pref.get();
+      expect(updateValue, false);
+
+      await pref.remove();
+      final deleteValue = await pref.get();
+      expect(deleteValue, null);
     });
 
-    test('値未設定の場合、デフォルト値が取得できる。', () async {
+    test('int', () async {
       SharedPreferences.setMockInitialValues({});
-      final beforeSave = await token.get();
-      expect(beforeSave, null);
+      final pref = TestPreference.intValue;
+      final startValue = await pref.get();
+      expect(startValue, null);
+
+      await pref.set(1);
+      final createValue = await pref.get();
+      expect(createValue, 1);
+
+      await pref.set(3);
+      final updateValue = await pref.get();
+      expect(updateValue, 3);
+
+      await pref.remove();
+      final deleteValue = await pref.get();
+      expect(deleteValue, null);
     });
 
-    test('値設定済みの場合、値が取得できる。', () async {
-      const expected = 'test';
-      SharedPreferences.setMockInitialValues({"backendToken": expected});
-      final result = await token.get();
-      expect(result, expected);
+    test('double', () async {
+      SharedPreferences.setMockInitialValues({});
+      final pref = TestPreference.doubleValue;
+      final startValue = await pref.get();
+      expect(startValue, null);
+
+      await pref.set(1.1);
+      final createValue = await pref.get();
+      expect(createValue, 1.1);
+
+      await pref.set(2.11);
+      final updateValue = await pref.get();
+      expect(updateValue, 2.11);
+
+      await pref.remove();
+      final deleteValue = await pref.get();
+      expect(deleteValue, null);
     });
 
-    test('値を削除できる。', () async {
-      SharedPreferences.setMockInitialValues({"backendToken": "test"});
-      final result = await token.remove();
-      expect(result, true);
+    test('string', () async {
+      SharedPreferences.setMockInitialValues({});
+      final pref = TestPreference.stringValue;
+      final startValue = await pref.get();
+      expect(startValue, null);
+
+      await pref.set('a');
+      final createValue = await pref.get();
+      expect(createValue, 'a');
+
+      await pref.set('bc');
+      final updateValue = await pref.get();
+      expect(updateValue, 'bc');
+
+      await pref.remove();
+      final deleteValue = await pref.get();
+      expect(deleteValue, null);
+    });
+
+    test('List<String>', () async {
+      SharedPreferences.setMockInitialValues({});
+      final pref = TestPreference.stringListValue;
+      final startValue = await pref.get();
+      expect(startValue, null);
+
+      await pref.set(['a', 'b']);
+      final createValue = await pref.get();
+      expect(createValue, ['a', 'b']);
+
+      await pref.set(['ac']);
+      final updateValue = await pref.get();
+      expect(updateValue, ['ac']);
+
+      await pref.remove();
+      final deleteValue = await pref.get();
+      expect(deleteValue, null);
     });
   });
 }
