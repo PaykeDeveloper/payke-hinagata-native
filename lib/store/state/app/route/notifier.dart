f3a3a5d630a3aa27db3d75d00e1b02c85532d434
urbsny <16968591+urbsny@users.noreply.github.com>
2021-04-23 13:00:11 +0900

    refactoring

--- lib/store/state/app/route/notifier.dart
+++ lib/store/state/app/route/notifier.dart
@@ -15,48 +15,48 @@ class RouteStateNotifier extends StateNotifier<RouteState> with LocatorMixin {
     );
   }
 
-  Future<List<Page>> push(BottomTab tab, Page page) async {
+  Future push(BottomTab tab, Page page) async {
     switch (tab) {
       case BottomTab.home:
         state = state.copyWith(
           homePages: [...state.homePages, page],
         );
-        return state.homePages;
+        break;
       case BottomTab.books:
         state = state.copyWith(
           bookPages: [...state.bookPages, page],
         );
-        return state.bookPages;
+        break;
     }
   }
 
-  Future<List<Page>> pop(BottomTab tab) async {
+  Future pop(BottomTab tab) async {
     switch (tab) {
       case BottomTab.home:
         state = state.copyWith(
           homePages: state.homePages.toList()..removeLast(),
         );
-        return state.homePages;
+        break;
       case BottomTab.books:
         state = state.copyWith(
           bookPages: state.bookPages.toList()..removeLast(),
         );
-        return state.bookPages;
+        break;
     }
   }
 
-  Future<List<Page>> replace(BottomTab tab, List<Page> pages) async {
+  Future replace(BottomTab tab, List<Page> pages) async {
     switch (tab) {
       case BottomTab.home:
         state = state.copyWith(
           homePages: pages,
         );
-        return state.homePages;
+        break;
       case BottomTab.books:
         state = state.copyWith(
           bookPages: pages,
         );
-        return state.bookPages;
+        break;
     }
   }
 }
