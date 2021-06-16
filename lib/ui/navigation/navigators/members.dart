4384011d4cf20e3c7267e8bbffc34ad0056a6121
ameyamashiro <yamashiro7044@gmail.com>
2021-06-16 11:36:18 +0900

    fix path

--- lib/ui/navigation/navigators/members.dart
+++ lib/ui/navigation/navigators/members.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:native_app/store/state/app/route/models/route_state.dart';
 import 'package:native_app/store/state/app/route/notifier.dart';
 import 'package:native_app/store/state/ui/division_id/selectors.dart';
-import 'package:native_app/ui/screens/division/Members/list.dart';
+import 'package:native_app/ui/screens/division/members/list.dart';
 import 'package:provider/provider.dart';
 
 class MembersNavigator extends StatelessWidget {
