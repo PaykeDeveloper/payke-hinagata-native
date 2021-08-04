19f9cad95c5093c12fc6c3486d461268584e8f4c
urbsny <16968591+urbsny@users.noreply.github.com>
2021-08-04 11:08:08 +0900

    use get_it

--- lib/base/constants.dart
+++ lib/base/constants.dart
@@ -1,4 +1,3 @@
 const productMode = bool.fromEnvironment('dart.vm.product');
 const debugPrintSize = !productMode && bool.fromEnvironment('DEBUG_PAINT_SIZE');
-const _backendOrigin = String.fromEnvironment('BACKEND_ORIGIN');
-final backendBaseUrl = _backendOrigin.isNotEmpty ? '$_backendOrigin/' : '';
+const backendBaseUrl = '${const String.fromEnvironment('BACKEND_ORIGIN')}/';
