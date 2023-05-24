b5a019eaadb5ffb1eb79eb1fa623e2ad4d3842e7
S.U <16968591+urbsny@users.noreply.github.com>
2023-05-24 19:41:12 +0900

    refactoring

--- lib/store/state/domain/division/members/models/member_input.dart
+++ lib/store/state/domain/division/members/models/member_input.dart
@@ -1,6 +1,7 @@
 // FIXME: SAMPLE CODE
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:native_app/store/base/models/json_generator.dart';
+import 'package:native_app/store/state/domain/common/users/models/user_id.dart';
 
 part 'member_input.freezed.dart';
 part 'member_input.g.dart';
@@ -8,7 +9,7 @@ part 'member_input.g.dart';
 @freezed
 class MemberInput with _$MemberInput implements JsonGenerator {
   const factory MemberInput({
-    required int userId,
+    @UserIdConverter() required UserId userId,
     required List<String> roleNames,
   }) = _MemberInput;
 
